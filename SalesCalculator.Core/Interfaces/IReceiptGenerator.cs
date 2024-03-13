a70afea6214c8b77c7c396e193374bde38ce9cb9
Mcebisi Mkhohliwe <mcebisimkhohliwe@gmail.com>
2024-03-13 22:49:59 +0200

    Added service to generate receipt

---
+++ SalesCalculator.Core/Interfaces/IReceiptGenerator.cs
@@ -0,0 +1,10 @@
+﻿using SalesCalculator.Common.Models.Request;
+using SalesCalculator.Common.Models.Response;
+
+namespace SalesCalculator.Core.Interfaces
+{
+    public interface IReceiptGenerator
+    {
+        Receipt Generate(List<ShoppingBasketItem> items);
+    }
+}
---
+++ SalesCalculator.Core/Services/ReceiptGenerator.cs
@@ -0,0 +1,39 @@
+﻿using SalesCalculator.Common.Models.Request;
+using SalesCalculator.Common.Models.Response;
+using SalesCalculator.Core.Interfaces;
+
+namespace SalesCalculator.Core.Services
+{
+    public class ReceiptGenerator : IReceiptGenerator
+    {
+        private readonly ITaxCalculatorService _taxCalculatorService;
+
+        public ReceiptGenerator(ITaxCalculatorService taxCalculatorService)
+        {
+            _taxCalculatorService = taxCalculatorService;
+        }
+
+        public Receipt Generate(List<ShoppingBasketItem> items)
+        {
+            var invoice = new Receipt();
+
+            items.ForEach(item =>
+            {
+                var productTaxAmount = _taxCalculatorService.Calculate(item.Price, item.HasExemption, item.IsImported);
+                var price = item.Price + productTaxAmount;
+
+                invoice.Items.Add(new ItemDTO
+                {
+                    Name = item.Name,
+                    Price = price,
+                    Quantity = item.Quantity
+                });
+
+                invoice.SalesTax += productTaxAmount;
+                invoice.Total += price;
+            });
+
+            return invoice;
+        }
+    }
+}
