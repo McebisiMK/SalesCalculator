6aeba3f67ec70d6c63fd2b9a0bca225be593ede3
Mcebisi Mkhohliwe <mcebisimkhohliwe@gmail.com>
2024-03-13 22:50:29 +0200

    Register services

---
+++ SalesCalculator.Core/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SalesCalculator.Common.Models.Configurations;
+using SalesCalculator.Core.Interfaces;
+using SalesCalculator.Core.Services;
+
+namespace SalesCalculator.Core
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection RegisterCore(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddOptions();
+            services.Configure<TaxOptions>(configuration.GetSection("TaxOptions"));
+            services.AddScoped<IReceiptGenerator, ReceiptGenerator>();
+            services.AddScoped<ITaxCalculatorService, TaxCalculatorService>();
+
+            return services;
+        }
+    }
+}
