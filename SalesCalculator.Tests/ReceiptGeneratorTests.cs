2c1658a95868bd0537088b706ee94d3035eaf652
Mcebisi Mkhohliwe <mcebisimkhohliwe@gmail.com>
2024-03-13 22:51:54 +0200

    receipt tests

---
+++ SalesCalculator.Tests/ReceiptGeneratorTests.cs
@@ -0,0 +1,96 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using SalesCalculator.Common.Models.Request;
+using SalesCalculator.Core.Interfaces;
+using SalesCalculator.Core.Services;
+using SalesCalculator.Tests.TestData;
+
+namespace SalesCalculator.Tests
+{
+    [TestFixture]
+    public class ReceiptGeneratorTests
+    {
+        private ITaxCalculatorService _taxCalculatorService;
+
+        [SetUp]
+        public void Setup()
+        {
+            var serviceProvider = TaxTestData.CreateServiceProvider();
+            _taxCalculatorService = serviceProvider.GetRequiredService<ITaxCalculatorService>();
+        }
+
+        [Test]
+        public void Generate_Given_Empty_List_Of_Shopping_Basket_Items_Should_Return_Empty_Receipt()
+        {
+            //---------------------------------- Arrange ----------------------------------
+            var shoppingBasket = new List<ShoppingBasketItem>();
+            var expectedReceipt = ReceiptTestData.GetEmptyReceipt();
+
+            var receiptGenerator = CreateReceiptGenerator();
+
+            //---------------------------------- Act     ----------------------------------
+            var actual = receiptGenerator.Generate(shoppingBasket);
+
+            //---------------------------------- Assert  ----------------------------------
+            actual.Items.Should().BeEmpty();
+            actual.Should().BeEquivalentTo(expectedReceipt);
+        }
+
+        [Test]
+        public void Generate_Given_First_List_Of_Shopping_Basket_Items_Should_Return_First_Receipt()
+        {
+            //---------------------------------- Arrange ----------------------------------
+            var shoppingBasket = ReceiptTestData.GetFirstInputShoppingBasket();
+            var expectedReceipt = ReceiptTestData.GetFirstOutputReceipt();
+
+            var receiptGenerator = CreateReceiptGenerator();
+
+            //---------------------------------- Act     ----------------------------------
+            var actual = receiptGenerator.Generate(shoppingBasket);
+
+            //---------------------------------- Assert  ----------------------------------
+            actual.Items.Should().HaveCount(shoppingBasket.Count);
+            actual.Should().BeEquivalentTo(expectedReceipt);
+        }
+
+        [Test]
+        public void Generate_Given_Second_List_Of_Shopping_Basket_Items_Should_Return_Second_Receipt()
+        {
+            //---------------------------------- Arrange ----------------------------------
+            var shoppingBasket = ReceiptTestData.GetSecondInputShoppingBasket();
+            var expectedReceipt = ReceiptTestData.GetSecondOutputReceipt();
+
+            var receiptGenerator = CreateReceiptGenerator();
+
+            //---------------------------------- Act     ----------------------------------
+            var actual = receiptGenerator.Generate(shoppingBasket);
+
+            //---------------------------------- Assert  ----------------------------------
+            actual.Items.Should().HaveCount(shoppingBasket.Count);
+            actual.Should().BeEquivalentTo(expectedReceipt);
+        }
+
+        [Test]
+        public void Generate_Given_Third_List_Of_Shopping_Basket_Items_Should_Return_Third_Receipt()
+        {
+            //---------------------------------- Arrange ----------------------------------
+            var shoppingBasket = ReceiptTestData.GetThirdInputShoppingBasket();
+            var expectedReceipt = ReceiptTestData.GetThirdOutputReceipt();
+
+            var receiptGenerator = CreateReceiptGenerator();
+
+            //---------------------------------- Act     ----------------------------------
+            var actual = receiptGenerator.Generate(shoppingBasket);
+
+            //---------------------------------- Assert  ----------------------------------
+            actual.Items.Should().HaveCount(shoppingBasket.Count);
+            actual.Should().BeEquivalentTo(expectedReceipt);
+        }
+
+        private ReceiptGenerator CreateReceiptGenerator()
+        {
+            return new ReceiptGenerator(_taxCalculatorService);
+        }
+    }
+}
---
+++ SalesCalculator.Tests/TestData/ReceiptTestData.cs
@@ -0,0 +1,196 @@
+﻿using SalesCalculator.Common.Models.Request;
+using SalesCalculator.Common.Models.Response;
+
+namespace SalesCalculator.Tests.TestData
+{
+    public class ReceiptTestData
+    {
+        public static Receipt GetEmptyReceipt()
+        {
+            return new Receipt();
+        }
+
+        public static List<ShoppingBasketItem> GetFirstInputShoppingBasket()
+        {
+            return new List<ShoppingBasketItem>
+            {
+                new ShoppingBasketItem
+                {
+                    Name = "Book",
+                    Quantity = 1,
+                    Price = 12.49M,
+                    HasExemption = true,
+                    IsImported = false
+                },
+                new ShoppingBasketItem
+                {
+                    Name = "Music CD",
+                    Quantity = 1,
+                    Price = 14.99M,
+                    HasExemption = false,
+                    IsImported = false
+                },
+                new ShoppingBasketItem
+                {
+                    Name = "Chocolate bar",
+                    Quantity = 1,
+                    Price = 0.85M,
+                    HasExemption = true,
+                    IsImported = false
+                }
+            };
+        }
+
+        public static Receipt GetFirstOutputReceipt()
+        {
+            return new Receipt
+            {
+                Items = new List<ItemDTO>
+                {
+                    new ItemDTO
+                    {
+                        Name = "Book",
+                        Quantity = 1,
+                        Price = 12.49M
+                    },
+                    new ItemDTO
+                    {
+                        Name = "Music CD",
+                        Quantity = 1,
+                        Price = 16.49M
+                    },
+                    new ItemDTO
+                    {
+                        Name = "Chocolate bar",
+                        Quantity = 1,
+                        Price = 0.85M
+                    }
+                },
+                SalesTax = 1.50M,
+                Total = 29.83M
+            };
+        }
+
+        public static List<ShoppingBasketItem> GetSecondInputShoppingBasket()
+        {
+            return new List<ShoppingBasketItem>
+            {
+                new ShoppingBasketItem
+                {
+                    Name = "Imported box of chocolates",
+                    Quantity = 1,
+                    Price = 10.00M,
+                    HasExemption = true,
+                    IsImported = true
+                },
+                new ShoppingBasketItem
+                {
+                    Name = "Imported bottle of perfume",
+                    Quantity = 1,
+                    Price = 47.50M,
+                    HasExemption = false,
+                    IsImported = true
+                }
+            };
+        }
+
+        public static Receipt GetSecondOutputReceipt()
+        {
+            return new Receipt
+            {
+                Items = new List<ItemDTO>
+                {
+                    new ItemDTO
+                    {
+                        Name = "Imported box of chocolates",
+                        Quantity = 1,
+                        Price = 10.50M
+                    },
+                    new ItemDTO
+                    {
+                        Name = "Imported bottle of perfume",
+                        Quantity = 1,
+                        Price = 54.63M
+                    }
+                },
+                SalesTax = 7.63M,
+                Total = 65.13M
+            };
+        }
+
+        public static List<ShoppingBasketItem> GetThirdInputShoppingBasket()
+        {
+            return new List<ShoppingBasketItem>
+            {
+                new ShoppingBasketItem
+                {
+                    Name = "Imported bottle of perfume",
+                    Quantity = 1,
+                    Price = 27.99M,
+                    HasExemption = false,
+                    IsImported = true
+                },
+                new ShoppingBasketItem
+                {
+                    Name = "Bottle of perfume",
+                    Quantity = 1,
+                    Price = 18.99M,
+                    HasExemption = false,
+                    IsImported = false
+                },
+                new ShoppingBasketItem
+                {
+                    Name = "Packet of paracetamol",
+                    Quantity = 1,
+                    Price = 9.75M,
+                    HasExemption = true,
+                    IsImported = false
+                },
+                new ShoppingBasketItem
+                {
+                    Name = "Box of imported chocolates",
+                    Quantity = 1,
+                    Price = 11.25M,
+                    HasExemption = true,
+                    IsImported = true
+                }
+            };
+        }
+
+        public static Receipt GetThirdOutputReceipt()
+        {
+            return new Receipt
+            {
+                Items = new List<ItemDTO>
+                {
+                    new ItemDTO
+                {
+                    Name = "Imported bottle of perfume",
+                    Quantity = 1,
+                    Price = 32.19M
+                },
+                new ItemDTO
+                {
+                    Name = "Bottle of perfume",
+                    Quantity = 1,
+                    Price = 20.89M
+                },
+                new ItemDTO
+                {
+                    Name = "Packet of paracetamol",
+                    Quantity = 1,
+                    Price = 9.75M
+                },
+                new ItemDTO
+                {
+                    Name = "Box of imported chocolates",
+                    Quantity = 1,
+                    Price = 11.81M
+                }
+                },
+                SalesTax = 6.66M,
+                Total = 74.64M
+            };
+        }
+    }
+}
